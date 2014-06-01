31f4ab9c5c73b4bbfe147836194b61525dd6281c
Simon Li <spli@dundee.ac.uk>
2014-06-01 17:16:37 +0100

    Put create/query MapAnnotations in a class, add namespace.

--- features/OmeroMetadata.py
+++ features/OmeroMetadata.py
@@ -23,80 +23,96 @@
 OMERO.features metadata storage
 
 At present only strings are supported for keys and values
-
-Todo: Namespace
 """
 
 import omero
 import omero.gateway
 from omero.rtypes import wrap, unwrap
 
-def create_map_ann(**kwargs):
-    d = dict((k, wrap(str(v))) for k, v in kwargs.iteritems())
-    m = omero.model.MapAnnotationI()
-    m.setMapValue(d)
-    m = sess.getUpdateService().saveAndReturnObject(m)
-    return unwrap(m.id)
 
+class MapAnnotations(object):
 
-def query_by_map_ann(**kwargs):
-    params = omero.sys.ParametersI()
+    def __init__(self, session, namespace=None):
+        self.session = session
+        self.namespace = namespace
+        if namespace is not None and not isinstance(namespace, str):
+            raise Exception('namespace must be a string')
 
-    qs = sess.getQueryService()
-    conditions = []
-    for k, v in kwargs.iteritems():
-        paramk = 'k%d' % len(conditions)
-        paramv = 'v%d' % len(conditions)
-        params.addString(paramk, k)
+    def create_map_ann(self, **kwargs):
+        d = dict((k, wrap(str(v))) for k, v in kwargs.iteritems())
+        m = omero.model.MapAnnotationI()
+        m.setNs(wrap(self.namespace))
+        m.setMapValue(d)
+        m = self.session.getUpdateService().saveAndReturnObject(m)
+        return unwrap(m.id)
 
-        if isinstance(v, list):
-            conditions.append('ann.mapValue[:%s] in (:%s)' % (paramk, paramv))
+    def query_by_map_ann(self, **kwargs):
+        params = omero.sys.ParametersI()
+
+        qs = self.session.getQueryService()
+        conditions = []
+
+        if self.namespace is not None:
+            conditions.append('ann.ns = :ns')
+            params.addString('ns', self.namespace)
+
+        for k, v in kwargs.iteritems():
+            paramk = 'k%d' % len(conditions)
+            paramv = 'v%d' % len(conditions)
+            params.addString(paramk, k)
+
+            if isinstance(v, list):
+                conditions.append(
+                    'ann.mapValue[:%s] in (:%s)' % (paramk, paramv))
+            else:
+                conditions.append(
+                    'ann.mapValue[:%s] = :%s' % (paramk, paramv))
+            params.add(paramv, wrap(v))
+
+        # join fetch mapValue is only needed if we need to return the map, the
+        # query should run without it
+        q = 'from MapAnnotation ann join fetch ann.mapValue map'
+        if conditions:
+            q += ' where ' + ' and '.join(conditions)
+
+        print q
+        results = qs.findAllByQuery(q, params)
+        return results
+
+    @classmethod
+    def type_to_str(x):
+        t = type(x)
+        if t in [bool, float, int, long, str]:
+            return '%s:%s' % (t.__name__, x)
         else:
-            conditions.append('ann.mapValue[:%s] = :%s' % (paramk, paramv))
-        params.add(paramv, wrap(v))
+            raise Exception('Unsupported type: %s' % t)
 
-    # join fetch mapValue is only needed if we need to return the map, the
-    # query should run without it
-    q = 'from MapAnnotation ann join fetch ann.mapValue map'
-    if conditions:
-        q += ' where ' + ' and '.join(conditions)
+    @classmethod
+    def type_from_str(s):
+        t, x = s.split(':', 1)
+        if t == 'bool':
+            if x == 'True':
+                return True
+            if x == 'False':
+                return False
+            raise Exception('Invalid bool: %s' % x)
+        if t == 'float':
+            return float(x)
+        if t == 'int':
+            return int(x)
+        if t == 'long':
+            return long(x)
+        if t == 'str':
+            return x
+        raise Exception('Unsupported type: %s' % s)
 
-    print q
-    results = qs.findAllByQuery(q, params)
-    return results
+    def create_map_ann_multitype(self, **kwargs):
+        d = dict((k, type_to_str(v)) for k, v in kwargs.iteritems())
+        return self.create_map_ann(**d)
 
-def type_to_str(x):
-    t = type(x)
-    if t in [bool, float, int, long, str]:
-        return '%s:%s' % (t.__name__, x)
-    else:
-        raise Exception('Unsupported type: %s' % t)
-
-def type_from_str(s):
-    t, x = s.split(':', 1)
-    if t == 'bool':
-        if x == 'True':
-            return True
-        if x == 'False':
-            return False
-        raise Exception('Invalid bool: %s' % x)
-    if t == 'float':
-        return float(x)
-    if t == 'int':
-        return int(x)
-    if t == 'long':
-        return long(x)
-    if t == 'str':
-        return x
-    raise Exception('Unsupported type: %s' % s)
-
-def create_map_ann_multitype(**kwargs):
-    d = dict((k, type_to_str(v)) for k, v in kwargs.iteritems())
-    return create_map_ann(**d)
-
-def query_by_map_ann_multitype(**kwargs):
-    d = dict((k, type_to_str(v)) for k, v in kwargs.iteritems())
-    return query_by_map_ann(**d)
+    def query_by_map_ann_multitype(self, **kwargs):
+        d = dict((k, type_to_str(v)) for k, v in kwargs.iteritems())
+        return self.query_by_map_ann(**d)
 
 
 # E.g.
