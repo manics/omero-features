b19a40d6317bd0d1f0ddcc8125861700ab38ea0e
Simon Li <spli@dundee.ac.uk>
2014-06-10 22:05:43 +0100

    Started work on storing metadata as MapAnnotation, data in tables

---
+++ features/OmeroTablesFeatureStore.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (C) 2014 University of Dundee & Open Microscopy Environment.
+# All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+"""
+Use OMERO MapAnnotations for metadata, store only the features in HDF5 tables
+"""
+
+from AbstractAPI import AbstractFeatureSetStorage, AbstractFeatureStorage
+import OmeroMetadata
+import omero
+from omero.rtypes import unwrap
+
+import itertools
+#import numpy
+#import os
+
+
+class FeatureSetTableStore(AbstractFeatureSetStorage):
+    """
+    A single feature set.
+    Each element is a fixed width array of doubles
+    """
+
+    def __init__(self, session, namespace, fsmeta):
+        self.session = session
+        self.ma = OmeroMetadata.MapAnnotations(session, namespace)
+        self.fsmeta = fsmeta
+        self.table = None
+        self.header = None
+        self.get_table()
+
+    def close(self):
+        if self.table:
+            self.table.close()
+            self.table = None
+
+    def get_table(self):
+        if self.table:
+            assert self.cols
+            return self.table
+        a = self.ma.query_by_map_ann(**dict(self.fsmeta.items()))
+        if len(a) < 1:
+            raise Exception(
+                'No annotations found for: %s' % str(self.fsmeta))
+        if len(a) > 1:
+            raise Exception(
+                'Multiple annotations found for: %s' % str(self.fsmeta))
+        return self.open_table(unwrap(a[0].getMapValue()['_tableid']))
+
+    def open_table(self, tid):
+        self.table = self.session.sharedResources().openTable(
+            omero.model.OriginalFileI(tid))
+        if not self.table:
+            raise Exception('Failed to open table ID:%d' % tid)
+        self.cols = self.table.getHeaders()
+        if not self.cols:
+            raise Exception('Failed to get columns for table ID:%d' % tid)
+
+    def store1(self, rowmeta, values):
+        # TODO Check for existing annotation
+        # TODO Check only one row in values
+        off = self.table.getNumberOfRows()
+        for n in xrange(len(self.cols)):
+            self.cols[n].values = values[n]
+        self.table.addData(self.cols)
+        tid = unwrap(self.get_table().getOriginalFile().getId())
+        self.ma.create_map_ann(**dict(
+            [('_tableid', tid), ('_offset', off)] + rowmeta.items()))
+
+    def store(self, rowmetas, values):
+        for (rowmeta, value) in itertools.izip(rowmetas, values):
+            self.store1(rowmeta, value)
+
+    def fetch(self, rowquery):
+        tid = unwrap(self.get_table().getOriginalFile().getId())
+        anns = self.ma.query_by_map_ann(**dict(
+            rowquery.items() + [('_tableid', tid)]))
+        offs = [unwrap(a.getMapValue()['_offset']) for a in anns]
+        data = self.table.readCoordinates(offs)
+        values = [c.values for c in data.columns]
+        return values
+
+
+class LRUCache(object):
+    """
+    A naive least-recently-used cache. Removal is O(n)
+    TODO: Improve efficiency
+    """
+
+    def __init__(self, size):
+        self.maxsize = size
+        self.cache = {}
+        self.counter = 0
+
+    def __len__(self):
+        return len(self.cache)
+
+    def get(self, key, miss=None):
+        try:
+            v = self.cache[key]
+            return v[0]
+        except KeyError:
+            return miss
+
+    def insert(self, key, value):
+        if key not in self.cache and len(self.cache) >= self.maxsize:
+            self.remove_oldest()
+        self.counter += 1
+        self.cache[key] = [value, self.counter]
+
+    def remove_oldest(self):
+        key = None
+        c = self.counter
+        for k, v in self.cache.iteritems():
+            if v[1] < c:
+                c = v[1]
+                key = k
+        del self.cache[key]
+
+
+class LRUTableCache(LRUCache):
+    """
+    Automatically closes a table handle when it is removed from the cache
+    """
+    def remove_oldest(self):
+        key = None
+        c = self.counter
+        for k, v in self.cache.iteritems():
+            if v[1] < c:
+                c = v[1]
+                key = k
+        self.cache[key].close()
+        del self.cache[key]
+
+
+class FeatureTableStore(AbstractFeatureStorage):
+    """
+    Manage storage of feature files and feature-set/context metadata
+    """
+
+    def __init__(self, session, **kwargs):
+        self.session = session
+        self.namespace = kwargs.get('namespace', 'omero.features')
+        self.cachesize = kwargs.get('cachesize', 10)
+        self.ma = OmeroMetadata.MapAnnotations(session, self.namespace)
+        self.fss = {}
+
+    def get_feature_set(self, fsmeta):
+        fskey = tuple(sorted(fsmeta.iteritems()))
+        if fskey not in self.fss:
+            self.fss[fskey] = FeatureSetTableStore(fsmeta)
+        return self.fss[fskey]
+
+    def store(self, fsmetas, rowmetas, values):
+        for fsmeta, values in itertools.izip(fsmetas, values):
+            fs = self.get_feature_set(fsmeta)
+            fs.store(rowmetas, values)
+
+    def fetch(self, fsquery, rowquery):
+        fs = self.get_feature_set(fsquery)
+        return fs.fetch(rowquery)
---
+++ test/unit/test_table_feature_store.py
@@ -0,0 +1,323 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# Copyright (C) 2014 University of Dundee & Open Microscopy Environment
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+import pytest
+import mox
+from copy import deepcopy
+
+import numpy
+import os
+
+import omero
+from omero.rtypes import unwrap, wrap
+
+from features import OmeroExternalFeatureStore
+from features import OmeroMetadata
+from features import OmeroTablesFeatureStore
+
+
+class TestLRUCache(object):
+
+    def test_get_insert_remove_oldest(self):
+        c = OmeroTablesFeatureStore.LRUCache(2)
+        assert len(c) == 0
+
+        assert c.get('key1') == None
+        assert c.get('key1', -1) == -1
+
+        c.insert('key1', 1)
+        assert c.get('key1') == 1
+        assert len(c) == 1
+
+        c.insert('key1', 'a')
+        assert c.get('key1') == 'a'
+        assert len(c) == 1
+
+        c.insert('key2', 2)
+        assert c.get('key1') == 'a'
+        assert c.get('key2') == 2
+        assert len(c) == 2
+
+        c.insert('key3', 3)
+        assert c.get('key1') == None
+        assert c.get('key2') == 2
+        assert c.get('key3') == 3
+        assert len(c) == 2
+
+
+class MockMapAnnotation:
+    def __init__(self, map):
+        self.map = dict((k, wrap(v)) for k, v in map.iteritems())
+
+    def getMapValue(self):
+        return self.map
+
+class MockSharedResources:
+    def __init__(self, tid, table):
+        self.tid = tid
+        self.table = table
+
+    def openTable(self, o):
+        assert isinstance(o, omero.model.OriginalFileI)
+        assert unwrap(o.id) == self.tid
+        return self.table
+
+class MockSession:
+    def __init__(self, tid, table):
+        self.msr = MockSharedResources(tid, table)
+
+    def sharedResources(self):
+        return self.msr
+
+class MockOriginalFile:
+    def __init__(self, id):
+        self.id = wrap(id)
+
+    def getId(self):
+        return self.id
+
+class MockColumn:
+    values = None
+
+    def __eq__(self, o):
+        return self.values == o.values
+
+class MockTableData:
+    columns = None
+
+class MockTable:
+    def __init__(self):
+        pass
+
+    def addData(self, cols):
+        pass
+
+    def close(self):
+        pass
+
+    def getNumberOfRows(self):
+        pass
+
+    def getOriginalFile(self):
+        pass
+
+    def readCoordinates(self):
+        pass
+
+    def getHeaders(self):
+        pass
+
+class MockFeatureSetTableStore(OmeroTablesFeatureStore.FeatureSetTableStore):
+    def __init__(self, session, namespace, fsmeta):
+        self.session = session
+        self.ma = OmeroMetadata.MapAnnotations(session, namespace)
+        self.fsmeta = {'fsname': 'a'}
+        self.cols = None
+        self.table = None
+        self.header = None
+
+class TestFeatureSetFileStore(object):
+
+    def setup_method(self, method):
+        self.mox = mox.Mox()
+
+    def teardown_method(self, method):
+        self.mox.UnsetStubs()
+
+    def test_close(self):
+        table = self.mox.CreateMock(MockTable)
+        table.close()
+        store = MockFeatureSetTableStore(None, None, None)
+        store.table = table
+
+        self.mox.ReplayAll()
+
+        store.close()
+        self.mox.VerifyAll()
+
+    @pytest.mark.parametrize('opened', [True, False])
+    def test_get_table(self, opened):
+        ann = MockMapAnnotation({'fsname': 'a', '_tableid': 1})
+        store = MockFeatureSetTableStore(None, None, None)
+        self.mox.StubOutWithMock(store.ma, 'query_by_map_ann')
+        self.mox.StubOutWithMock(store, 'open_table')
+        table = self.mox.CreateMock(MockTable)
+
+        if opened:
+            store.table = table
+            store.cols = object()
+        else:
+            store.ma.query_by_map_ann(fsname='a').AndReturn([ann])
+            store.open_table(1).AndReturn(table)
+
+        self.mox.ReplayAll()
+
+        assert store.get_table() == table
+        self.mox.VerifyAll()
+
+    def test_open_table(self):
+        table = self.mox.CreateMock(MockTable)
+        session = MockSession(1, table)
+        store = MockFeatureSetTableStore(session, None, None)
+        cols = [object]
+
+        table.getHeaders().AndReturn(cols)
+        self.mox.ReplayAll()
+
+        store.open_table(1)
+        assert store.table == table
+        assert store.cols == cols
+        self.mox.VerifyAll()
+
+    def test_store1(self):
+        table = self.mox.CreateMock(MockTable)
+        store = MockFeatureSetTableStore(None, None, None)
+        self.mox.StubOutWithMock(store.ma, 'create_map_ann')
+
+        store.table = table
+        cols = [MockColumn]
+        cols[0].values = [2]
+        store.cols = [MockColumn]
+
+        table.getNumberOfRows().AndReturn(1)
+        table.addData(cols)
+        table.getOriginalFile().AndReturn(MockOriginalFile(3))
+
+        d = {'_tableid': 3, '_offset': 1, 'objectid': 4}
+        store.ma.create_map_ann(**d)
+        rowmeta = {'objectid': 4}
+        self.mox.ReplayAll()
+
+        store.store1(rowmeta, [2])
+        self.mox.VerifyAll()
+
+    def test_store(self):
+        store = MockFeatureSetTableStore(None, None, None)
+        self.mox.StubOutWithMock(store, 'store1')
+        rowmetas = [{'objectid': 4}, {'objectid': 5}]
+        values = [[1], [2]]
+
+        store.store1(rowmetas[0], values[0])
+        store.store1(rowmetas[1], values[1])
+        self.mox.ReplayAll()
+
+        store.store(rowmetas, values)
+        self.mox.VerifyAll()
+
+    def test_fetch(self):
+        ann1 = MockMapAnnotation({'name': 'a', '_tableid': 1, '_offset': 1})
+        ann2 = MockMapAnnotation({'name': 'a', '_tableid': 1, '_offset': 6})
+        table = self.mox.CreateMock(MockTable)
+        store = MockFeatureSetTableStore(None, None, None)
+        store.table = table
+        store.cols = [MockColumn()]
+
+        self.mox.StubOutWithMock(table, 'getOriginalFile')
+        table.getOriginalFile().AndReturn(MockOriginalFile(1))
+        self.mox.StubOutWithMock(store.ma, 'query_by_map_ann')
+
+        rowquery = {'name': 'a'}
+        d = dict(rowquery.items() + [('_tableid', 1)])
+        store.ma.query_by_map_ann(**d).AndReturn([ann1, ann2])
+
+        self.mox.StubOutWithMock(table, 'readCoordinates')
+        values = [10, 20]
+        data = MockTableData()
+        col = MockColumn()
+        col.values = values
+        data.columns = [col]
+        table.readCoordinates([1, 6]).AndReturn(data)
+
+        self.mox.ReplayAll()
+
+        assert store.fetch(rowquery) == [values]
+        self.mox.VerifyAll()
+
+
+class TestFeatureTableStore(object):
+
+    def setup_method(self, method):
+        self.mox = mox.Mox()
+        self.fsmeta = {'name': 'a', 'version': '1'}
+        self.rowmetas = [{'id': '1'}, {'id': '2'}]
+        self.valuess = [[1.0, 2.0], [-1.0, -2.0]]
+
+    def teardown_method(self, method):
+        self.mox.UnsetStubs()
+
+    @pytest.mark.parametrize('opened', [True, False])
+    def test_get_feature_set(self, opened):
+        fs = MockFeatureSetTableStore(None, None, None)
+        self.mox.StubOutWithMock(
+            OmeroTablesFeatureStore, 'FeatureSetTableStore')
+        fsmeta = {'version': '1.2.3', 'fsname': 'a'}
+        fskey = (('fsname', 'a'), ('version', '1.2.3'))
+        fts = OmeroTablesFeatureStore.FeatureTableStore(None)
+
+        if opened:
+            fts.fss[fskey] = fs
+        else:
+            OmeroTablesFeatureStore.FeatureSetTableStore(fsmeta).AndReturn(fs)
+        self.mox.ReplayAll()
+
+        assert fts.get_feature_set(fsmeta) == fs
+        assert fts.fss.keys() == [fskey]
+        self.mox.VerifyAll()
+
+    def test_store(self):
+        fts = OmeroTablesFeatureStore.FeatureTableStore(None)
+        self.mox.StubOutWithMock(fts, 'get_feature_set')
+
+        fsmetas = [{'fsname': 'a'}, {'fsname': 'b', 'x': '1'}]
+        rowmetas = [{'objectid': 1}, {'objectid': 2}]
+        values1 = [[1], [2]]
+        values2 = [[3], [4]]
+
+        fs1 = MockFeatureSetTableStore(None, None, fsmetas[0])
+        fs2 = MockFeatureSetTableStore(None, None, fsmetas[1])
+        self.mox.StubOutWithMock(fs1, 'store')
+        self.mox.StubOutWithMock(fs2, 'store')
+
+        fts.get_feature_set(fsmetas[0]).AndReturn(fs1)
+        fs1.store(rowmetas, values1)
+        fts.get_feature_set(fsmetas[1]).AndReturn(fs2)
+        fs2.store(rowmetas, values2)
+        self.mox.ReplayAll()
+
+        fts.store(fsmetas, rowmetas, [values1, values2])
+        self.mox.VerifyAll()
+
+    def test_fetch(self):
+        fts = OmeroTablesFeatureStore.FeatureTableStore(None)
+        self.mox.StubOutWithMock(fts, 'get_feature_set')
+        fsquery = {'name': 'a'}
+        rowquery = {'objectid': 1}
+        values = [[1, 2]]
+        fs = MockFeatureSetTableStore(None, None, fsquery)
+        self.mox.StubOutWithMock(fs, 'fetch')
+
+        fts.get_feature_set(fsquery).AndReturn(fs)
+        fs.fetch(rowquery).AndReturn(values)
+
+        self.mox.ReplayAll()
+
+        assert fts.fetch(fsquery, rowquery) == values
+        self.mox.VerifyAll()
