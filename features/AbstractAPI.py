096369a8f4cfc3dc7d2971782953c41058baa4b3
Simon Li <spli@dundee.ac.uk>
2014-09-19 18:18:17 +0100

    Converted AbstractAPI to that envisaged for version 1

--- features/AbstractAPI.py
+++ features/AbstractAPI.py
@@ -24,70 +24,142 @@ OMERO.features abstract API
 """
 
 
-class AbstractFeatureSetStorage(object):
+class FeatureRow(object):
     """
-    A single feature set.
-    Each element is a fixed width array of doubles
+    A featureset row
+
+    Each row consists of a list of arrays of doubles
     """
 
-    def store1(self, rowmeta, values):
+    def __init__(self, widths=None, names=None, values=None):
+        assert widths or values
+
+        self._widths = widths
+        self._names = None
+        self._values = None
+        if values:
+            self.values = values
+        if names:
+            assert len(names) == len(self._widths)
+        self._names = names
+        self._namemap = {}
+
+    def get_index(self, name):
+        try:
+            return self._namemap[name]
+        except KeyError:
+            self._namemap = dict(
+                ni for ni in zip(self._names, xrange(len(self._names))))
+            return self._namemap[name]
+
+    def __getitem__(self, key):
+        return self.values[self.get_index(key)]
+
+    def __setitem__(self, key, value):
+        i = self.get_index(key)
+        assert len(value) == self._widths[i]
+        self.values[i] = value
+
+    @property
+    def names(self):
+        return self._names
+
+    @property
+    def widths(self):
+        return self._widths
+
+    @property
+    def values(self):
+        return self._values
+
+    @values.setter
+    def values(self, value):
+        if self._names:
+            assert len(self.names) == len(self._values)
+        widths = [len(v) for v in value]
+        if self._widths:
+            assert self._widths == widths
+        else:
+            self._widths = widths
+        self._values = value
+
+    @values.deleter
+    def values(self):
+        del self._values
+
+    def __repr__(self):
+        return '%s(widths=%r, names=%r, values=%r)' % (
+            self.__class__.__name__, self._widths, self._names, self._values)
+
+
+
+
+
+class AbstractFeatureStorageManager(object):
+    """
+    Manages multiple feature stores
+
+    Each entry in a feature store consists of a FeatureRow
+    """
+
+    def create(self, featureset_name, names, widths):
         """
-        Store a single feature set for a single sample
+        Create a new feature store
 
-        :param rowmeta: A dictionary of sample metadata
-        :values: A list of double arrays [[...], [...], ...], one per column
-            in this feature-set
+        :param featureset_name: The featureset identifier
+        :param names: A list of feature names
+        :param widths: A list of widths of each feature
         """
         raise Exception('Not implemented')
 
-    def store(self, rowmetas, values):
+    def store(self, featureset_name, image_id, roi_id, values):
         """
-        Store a single feature set for multiple samples
+        Store a row of features identified by Image ID and/or ROI ID
 
-        :param rowmeta: A list of dictionaries of sample metadata
-        :values: A list of list of double arrays [[[...], [...], ...], ...],
-            corresponding to the sample, then featureset-column
-        """
-        raise Exception('Not implemented')
-
-    def fetch(self, rowquery):
-        """
-        Retrieve a set of features for any samples matching the query
-
-        :param rowquery: A dictionary of keys/values to query
-        :return: [metadata-dicts], [([double], ...), ...]
-           A list of metadata dictionary, and a list of tuples of
-           double-arrays. Each tuple holds the feature-set for a sample,
-           and is a list of double-arrays
+        :param featureset_name: The featureset identifier
+        :param image_id: The Image ID
+        :param roi_id: The ROI ID, may be None
+        :params values: A list of FeatureRows
         """
         raise Exception('Not implemented')
 
-
-class AbstractFeatureStorage(object):
-    """
-    Multiple feature sets.
-    """
-
-    def store(self, fsmeta, rowmeta, values):
+    def fetch_by_image(self, featureset_name, image_id):
         """
-        Store a single feature set for multiple samples
+        Retrieve a single FeatureRow by Image ID
 
-        :param fsmeta: A dictionary describing the required feature set
-        :param rowmeta: A list of dictionaries of sample metadata
-        :values: A list of list of double arrays [[[...], [...], ...], ...],
-            corresponding to the sample, then featureset-column
+        :param featureset_name: The featureset identifier
+        :param image_id: The Image ID
+        :return: A FeatureRow
         """
         raise Exception('Not implemented')
 
-    def fetch(self, fsmeta, rowmeta):
+    def fetch_by_roi(self, featureset_name, roi_id):
         """
-        Retrieve a set of features for any samples matching the query
+        Retrieve a single FeatureRow by ROI ID
 
-        :param fsmeta: A dictionary describing the required feature set
-        :param rowquery: A dictionary of keys/values to query
-        :return: [metadata-dicts], [([double], ...), ...]
-           A list of metadata dictionary, and a list of tuples of
-           double-arrays. Each tuple holds the feature-set for a sample,
-           and is a list of double-arrays
+        :param featureset_name: The featureset identifier
+        :param roi_id: The ROI ID
+        :return: A FeatureRow
+        """
+        raise Exception('Not implemented')
+
+    def fetch_all(self, featureset_name, image_id):
+        """
+        Retrieve all rows of features identified by Image ID
+
+        :param featureset_name: The featureset identifier
+        :param image_id: The Image ID
+        :return: A list of FeatureRows
+        """
+        raise Exception('Not implemented')
+
+
+    def filter(self, featureset_name, conditions):
+        """
+        Retrieve the features and Image/ROI IDs which fulfill the conditions
+
+        :param featureset_name: The featureset identifier
+        :param conditions: The feature query conditions
+        :return: A list of (Image-ID, ROI-ID, FeatureRow) triplets
         """
         raise Exception('Not implemented')
